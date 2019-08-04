48f53a601d1dd988d629649d20c13114bc76f369
Caro Berry <carolbpr@gmail.com>
2019-08-04 17:43:24 -0500

    Datepicker pluggin and some html/css done

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+// Start logic after page has loaded
+$(document).ready(function () {
+    // TODO
+});
+//This function is to pick the Date
+$(function() {
+    $('#popupDatepicker').datepick();
+});
